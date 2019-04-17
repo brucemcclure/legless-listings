49d83678969ffe8cbffbf48563d1a578b0c3c593
Bruce Mc Clure <brucemcc90@gmail.com>
2019-04-17 13:03:10 +1000

    Added in snake names gem and edited the seeds files to create breeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Snake.names.each { |name|
+    Breed.create(name: name.downcase.strip)
+    puts "#{name} created"
+}
